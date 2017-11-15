127a013421586d690da5e146351f7713c277be30
Aareon Sullivan <askully13@gmail.com>
2017-11-15 15:04:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from kyoukai import Kyoukai
 app = Kyoukai(">Terminal_ API")
 
 for cog in os.listdir("cogs"):
-    if not cog.endswith(".py"):
-        continue
-
-    lib = importlib.import_path(f"cogs.{cog}")
-    lib.setup(app)
+    if cog.endswith(".py"):
+        lib = importlib.import_path(f"cogs.{cog}")
+        lib.setup(app)
 
 app.run("0.0.0.0", 8001)
